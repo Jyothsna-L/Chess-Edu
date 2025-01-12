2af338d74e91f2275cf7ef393cae703cb717d4de
Jyothsna L <132186756+Jyothsna-L@users.noreply.github.com>
2025-01-12 18:40:56 +0530

    Update script2.js
    changed the conditions for OCR

--- script2.js
+++ script2.js
@@ -16,12 +16,12 @@ document.addEventListener("DOMContentLoaded", function () {
       const { data } = await Tesseract.recognize(file, "eng");
       console.log("Extracted Text:", data.text);
 
-      const validText1 = "PSGRK".toLowerCase();
-      const validText2 = "krishnammal".toLowerCase();
+      const validText1 = "STUDENT SMART ID;
+      const validText2 = "Krishnammal";
       const rollNumberPattern = /\b\d{2}[A-Z]{3}\d{3}\b/; // Regex for roll numbers like "00XYZ000"
 
       // Check for both the college name and a valid roll number
-      if ((data.text.includes(validText1) || data.text.includes(validText2)) && rollNumberPattern.test(data.text)) {
+      if ((data.text.includes(validText1) || data.text.includes(validText2)) || rollNumberPattern.test(data.text)) {
         resultElement.textContent = "Access Granted";
         localStorage.setItem("validated", "true");
         window.location.href = "index.html"; // Redirect to main.html
